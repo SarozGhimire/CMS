ce5a6ab807917ac16b3e4f914599effd2243f847
sarojghimire2074@gmail.com <sarojghimire2074@gmail.com>
2019-11-21 11:06:21 +0545

    first commit

--- includes/login.php
+++ includes/login.php
@@ -26,12 +26,11 @@ if (isset($_POST['login'])) {
 		$db_user_role = $row['user_role'];
 	}
 
-	$password = crypt($password, $db_user_password);
+	// $password = crypt($password, $db_user_password);
 
 
-	if ($username !== $db_username && $password !== $db_user_password) {
-		header("Location: ../index.php ");
-	}else if ($username == $db_username && $password == $db_user_password) {
+	
+	if (password_verify($password,$db_user_password)) {
 
 		$_SESSION['username'] = $db_username;
 		$_SESSION['user_firstname'] = $db_user_firstname;
--- registration.php
+++ registration.php
@@ -15,16 +15,18 @@ if (isset($_POST['submit'])) {
 		$email    = mysqli_real_escape_string($connection, $email);
 		$password = mysqli_real_escape_string($connection, $password);
 
-		$query = "SELECT randSalt FROM users";
-		$select_randsalt_query = mysqli_query($connection,$query);
-		if (!$select_randsalt_query) {
-			die("QUERY FAILED" . mysqli_error($connection));
-		}
+		$password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12) );
 
-		$row = mysqli_fetch_array($select_randsalt_query);
+		// $query = "SELECT randSalt FROM users";
+		// $select_randsalt_query = mysqli_query($connection,$query);
+		// if (!$select_randsalt_query) {
+		// 	die("QUERY FAILED" . mysqli_error($connection));
+		// }
 
-		$salt = $row['randSalt'];
-		$password = crypt($password, $salt);
+		// $row = mysqli_fetch_array($select_randsalt_query);
+
+		// $salt = $row['randSalt'];
+		// $password = crypt($password, $salt);
 
 		$query = "INSERT INTO users (username, user_email, user_password, user_role) ";
 		$query .= "VALUES ('{$username}', '{$email}', '{$password}', 'subscriber' )";
